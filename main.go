79776508f0f364d390798b65c1c422c9055ce8ac
Alina Maslo <alina-maslo@inbox.ru>
2017-10-01 16:09:12 +0100

    add func MsgPrintln

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	//"strings"
 )
 
+func MsgPrintln(msg string) {
+	fmt.Println("\033[1;92m" + msg + "\033[0m")
+}
+
 func main() {
+	MsgPrintln("test test test")
 	fmt.Println("%#v", os.Args)
 }
